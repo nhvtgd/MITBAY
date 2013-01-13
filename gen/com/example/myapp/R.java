d787e2b75b3a7eefa946469167001d9e0d8c5bec
Tran Nguyen <viettran@mit.edu>
2013-01-13 02:53:02 -0500

    delete

--- gen/com/example/myapp/R.java
+++ gen/com/example/myapp/R.java
@@ -8,24 +8,89 @@
 package com.example.myapp;
 
 public final class R {
+    public static final class anim {
+        public static final int custom_anim=0x7f040000;
+        public static final int fade_in=0x7f040001;
+        public static final int fade_in2=0x7f040002;
+        public static final int fade_out=0x7f040003;
+    }
     public static final class attr {
     }
+    public static final class color {
+        public static final int aqua=0x7f050009;
+        public static final int black=0x7f05000f;
+        public static final int blue=0x7f05000d;
+        public static final int fuchsia=0x7f050002;
+        public static final int gray=0x7f050005;
+        public static final int green=0x7f05000c;
+        public static final int lime=0x7f05000a;
+        public static final int maroon=0x7f050008;
+        public static final int navy=0x7f05000e;
+        public static final int olive=0x7f050006;
+        public static final int purple=0x7f050007;
+        public static final int red=0x7f050003;
+        public static final int silver=0x7f050004;
+        public static final int teal=0x7f05000b;
+        public static final int white=0x7f050000;
+        public static final int yellow=0x7f050001;
+    }
+    public static final class dimen {
+        public static final int description_size=0x7f060003;
+        /**  Splash Screen 
+         */
+        public static final int logo_size=0x7f060000;
+        public static final int version_size=0x7f060001;
+        public static final int version_spacing=0x7f060002;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bike=0x7f020000;
+        public static final int furniture=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int logo=0x7f020003;
+        public static final int miscellaneous=0x7f020004;
+        public static final int mit_logo=0x7f020005;
+        public static final int mit_seal=0x7f020006;
+        public static final int textbook=0x7f020007;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int TableLayout_Splash=0x7f0a0005;
+        public static final int TableRow01_Splash=0x7f0a0006;
+        public static final int TableRow02=0x7f0a0007;
+        public static final int bike_button=0x7f0a0002;
+        public static final int description_text=0x7f0a0008;
+        public static final int furniture_button=0x7f0a0001;
+        public static final int logo_image=0x7f0a0004;
+        public static final int menu_settings=0x7f0a000a;
+        public static final int miscellaneous_button=0x7f0a0003;
+        public static final int textbook_button=0x7f0a0000;
+        public static final int version_text=0x7f0a0009;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_item_selection=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_splash_screen=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_item_selection=0x7f090000;
+        public static final int activity_main=0x7f090001;
+        public static final int activity_splash_screen=0x7f090002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f070000;
+        public static final int bike_string=0x7f070006;
+        public static final int furniture_string=0x7f070005;
+        public static final int hello_world=0x7f070001;
+        /**  SplashScreen Page 
+         */
+        public static final int logo_description=0x7f070009;
+        public static final int menu_settings=0x7f070002;
+        public static final int miscellaneous_string=0x7f070007;
+        public static final int textbook_string=0x7f070004;
+        /**  Item Selection page String 
+         */
+        public static final int title_activity_item_selection=0x7f070003;
+        public static final int title_activity_splash_screen=0x7f070008;
+        public static final int version=0x7f07000a;
     }
     public static final class style {
         /** 
@@ -49,10 +114,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f080001;
     }
 }
