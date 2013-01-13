244fb29b1eed5dfa1e9956e037da967b7aceca0d
Duy <duyha@mit.edu>
2013-01-13 13:02:47 -0800

    Adding User, Sellable

---
+++ src/com/example/myapp/Sellable.java
@@ -0,0 +1,37 @@
+package com.example.myapp;
+
+import java.util.Date;
+
+public class Sellable {
+	private String name;
+	private String price;
+	private User user;
+	private Date date;
+	private String description;
+	
+	public enum Condition {NEW,	USED, ACCEPTABLE};
+	
+	
+	/**
+	 * 
+	 * @param user
+	 * @param name
+	 */
+	public Sellable(User user,String name){
+		
+	}
+	
+	/**
+	 * 
+	 * @param user
+	 * @param name
+	 * @param price
+	 */
+	
+	public Sellable(User user,String name,String price){
+		
+	}
+	
+	
+
+}
