8c9daeffbf8211eee13ba0614d7f83f5290080a3
Tran Nguyen <viettran@mit.edu>
2013-01-13 00:02:22 -0500

    Create the splash screen, item selection pages Change the intent if you want to transition to your page

--- src/com/example/myapp/MainActivity.java
+++ src/com/example/myapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	/*Just to transition to Item Selection Page, will change 
+	 * later*/
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		if (event.getAction() == MotionEvent.ACTION_DOWN){
+			Intent i = new Intent(this,ItemSelection.class);
+			startActivity(i);
+		}
+			
+		return true;
 	}
 
 	@Override
