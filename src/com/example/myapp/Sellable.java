ff4b8b7386c9d2882926342655897a2716994aab
Tiffany Tang <fable@mit.edu>
2013-01-15 22:27:32 -0500

    updated sellable and user with more stuff

--- src/com/example/myapp/Sellable.java
+++ src/com/example/myapp/Sellable.java
@@ -4,8 +4,9 @@ import java.util.Date;
 
 public class Sellable {
 	private String name;
+	private User seller;
 	private String price;
-	private User user;
+	private User buyer;
 	private Date date;
 	private String description;
 	private boolean enabled;
@@ -19,22 +20,25 @@ public class Sellable {
 	 * @param price
 	 */
 	
-	public Sellable(User user,String name,String price){
-		this.user = user;
+	public Sellable(User seller,String name,String price){
+		this.seller = seller;
 		this.name = name;
 		this.price = price;	
 		this.enabled = true;
+		this.buyer = null;
 	}
 	
 	public Double convertPriceToDouble(String price){
 		return null;
 	}
 	
-	public void disableSellable(){
+	public void disableSellable(User buyer){
+		this.buyer = buyer;
 		enabled = false;
 	}
 	
 	public void enableSellable(){
+		buyer = null;
 		enabled = true;
 	}
 	
--- src/com/example/myapp/User.java
+++ src/com/example/myapp/User.java
@@ -1,5 +1,13 @@
 package com.example.myapp;
 
 public class User {
-
+	private String name;
+	private String email;
+	private String password;
+	
+	public User(String name, String email, String password){
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
 }
