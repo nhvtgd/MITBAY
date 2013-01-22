7163176a0d9c9d79bc873cde5e321d5afe34c42d
Duy <duyha@mit.edu>
2013-01-22 04:57:45 -0800

    Item detail load information from previous activity

--- src/com/example/myapp/ItemDetail.java
+++ src/com/example/myapp/ItemDetail.java
@@ -1,10 +1,19 @@
 package com.example.myapp;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,7 +40,11 @@ public class ItemDetail extends MITBAYActivity {
 	 * Load picture from extras of the last axtivity
 	 */
 	public void loadPicture(Bundle bundle) {
-		
+//		Bitmap bitmap = getIntent().getParcelableExtra(IMAGE);
+		String imgPath = bundle.getString("imgPath").toString();
+		Bitmap bitmap = BitmapFactory.decodeFile(imgPath);
+		Log.d("Image", (bitmap==null) +", "+imgPath);
+		((ImageView) findViewById(R.id.ItemDetail_Piture)).setImageBitmap(bitmap);
 	}
 	/**
 	 * Load Item information from extras of the last activity
@@ -40,22 +53,31 @@ public class ItemDetail extends MITBAYActivity {
 	public void loadTextInformation(Bundle bundle) {
 		// Item name
 		((TextView) findViewById(R.id.ItemDetail_ItemName))
-		.setText(bundle.getString("item_name", "Anonymous"));
+		.setText(String.format("%s %n%s",
+				bundle.getString(ITEM, "No named").toString(),
+				bundle.getString(DATE, "")).toString());
 		// Category
 		((TextView) findViewById(R.id.ItemDetail_Category))
-		.setText(bundle.getString("category", "Not defined"));
+		.setText(bundle.getString(CONDITION, "").toString());
 		// Price
 		((TextView) findViewById(R.id.ItemDetail_Price))
-		.setText(bundle.getString("price", "No price"));;
+		.setText(bundle.getString(PRICE).toString());;
 		// Description
 		((TextView) findViewById(R.id.ItemDetail_Description))
-		.setText(bundle.getString("description", ""));
+		.setText(bundle.getString(DESCRIPTION, "").toString());
 		// Get seller information
 		String user_information = String.format("%s %n %s",
-				bundle.getString("username", "Anonymous"),
-				bundle.getString("email", "No email available"));
+				bundle.getString(USERNAME, "Anonymous").toString(),
+				bundle.getString(EMAIL, "No email available").toString());
 		((TextView) findViewById(R.id.ItemDetail_Seller)).
 								setText(user_information);
+		// Load category
+		String type = bundle.getString(TYPE).toString();
+		ImageView pic = (ImageView) findViewById(R.id.ItemDetail_ImageForItem);
+		if (type.equals(TEXTBOOK)) pic.setImageResource(R.drawable.textbook);
+		else if (type.equals(FURNITURE)) pic.setImageResource(R.drawable.furniture);
+		else if (type.equals(TRANSPORTATION)) pic.setImageResource(R.drawable.bike);
+		else pic.setImageResource(R.drawable.miscellaneous);
 	}
 	
 	/**
@@ -82,3 +104,30 @@ public class ItemDetail extends MITBAYActivity {
 		return true;
 	}
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	File file = new File(Environment.getExternalStorageDirectory(), "small_buy.png");
+//	Uri outputFileUri = Uri.fromFile(file);
+//	String imgPath = outputFileUri.getPath();
+//	try {
+//		FileOutputStream fOut = new FileOutputStream(file);
+//		item.getImages().compress(Bitmap.CompressFormat.PNG, 50, fOut);
+//		fOut.flush();
+//		fOut.close();
+//	} catch (Exception e) {}
+//	intent.putExtra("imgPath", imgPath);
+//}
