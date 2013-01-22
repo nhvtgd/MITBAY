02564f857be022da73c920474d09dc55e58745a6
Duy <duyha@mit.edu>
2013-01-22 12:13:45 -0800

    Add some final variables in MITBAYActivity

--- src/com/example/myapp/MITBAYActivity.java
+++ src/com/example/myapp/MITBAYActivity.java
@@ -14,7 +14,7 @@ public class MITBAYActivity extends Activity {
 
 	public static final String TEXTBOOK = "TEXTBOOK";
 	public static final String FURNITURE= "FURNITURE";
-	public static final String TRANSPORTATION = "FURNITURE";
+	public static final String TRANSPORTATION = "TRANSPORTATION";
 	public static final String MISC= "MISC";
 	public static final String USERNAME="username";
 	public static final String EMAIL="email";
@@ -26,6 +26,12 @@ public class MITBAYActivity extends Activity {
 	public static final String ITEM="item";
 	public static final String ID="id";
 	public static final String IMAGE="image";
+	// SharedPreferences settings = getSharePreferences(SETTING, 0);
+	public static final String SETTING = "setting";
+	public static final String NAME = "User name";
+	// EMAIL is already declared 
+	public static final String ADDRESS = "Address";
+	public static final String PASSWORD = "Password";
 	
 	
 	@Override
